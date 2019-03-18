1309b59d3256982a80d1c285f421ca43ee9c971f
PALLAVISK <pallavi.kadam@qed42.com>
2019-03-18 18:38:58 +0530

    browsync,image optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
 
 
-gulp.task('sass', defaultTask);
-function defaultTask(){
-    return gulp.src('app/scss/style.scss')
-      .pipe(sass()) // Converts Sass to CSS with gulp-sass
-      .pipe(gulp.dest('app/css'))
+gulp.task('sass', defaultTask); //converts sass to css files
+function defaultTask(done){
+    return gulp.src('custom/newboot_cdn/sass/style.scss')
+      .pipe(sass()) 
+      .pipe(gulp.dest('custom/newboot_cdn/css'))
       .pipe(browserSync.reload({
         stream: true
-      }))
-};
+      }));
+}
 
-gulp.task('browserSync',
-function () {
+gulp.task('browserSync',function () {  // browser sync help to sync so no need to reload page everytime
   browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
+    open: 'external',
+    hostname: 'localhost',
+    proxy: 'http://localhost:8888/drupal8-dev/web/',    
+  });
+})
+ 
+gulp.task('images', function(){  // it optimize the image according to the interface 
+  return gulp.src('custom/newboot_cdn/css/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('custom/newboot_cdn/myimages'))
 });
 
 gulp.task('hello',()=>{
@@ -27,12 +33,13 @@ gulp.task('hello',()=>{
   
 })
 
+gulp.task('watch', gulp.parallel('browserSync',function(done){
+  gulp.watch('custom/newboot_cdn/sass/style.scss', gulp.series('sass'));
+}))
 
 
-gulp.task('watch', function(){
-gulp.watch('app/scss/style.scss', gulp.series(defaultTask,'hello')); 
-});
 
-gulp.task('wat', ['browserSync'], function (){
-gulp.watch('app/scss/style.scss', gulp.series(defaultTask)); 
-});
+// gulp.task('watch', function(){
+// gulp.watch('custom/newboot_cdn/sass/style.scss', gulp.series(defaultTask,'hello')); 
+// });
+
